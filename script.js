c3fa2339821a018df65618becba9b2fe1a8e1b1b
JohnAntuch <johnantuch@gmail.com>
2022-03-21 11:01:20 -0300

    Update script.js
    More source codes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const secretNumber = Math.trunc (Math.random() * 20) +
-1;
+const secretNumber = Math.trunc (Math.random() * 20) + 1;
 
 let score = 20;
              
@@ -24,6 +23,30 @@ document.querySelector('.check').addEventlistener('click', function ()) {
 	
 	document.querySelector('.number').style.width =
 	'30rem';
-}
+} else if (guess > secretNumber) {
+	if (score > 1) {
+ 		document.querySelector('.message').textContent =
+      	'Too high!'; 
+ 		score-;
+ 	document.querySelector('.score').textContent =
+ 	score;
+ } else {
+ 	document.queSelector('.message').textContent
+ 	= 'You lost the game!';
+ 	document.querySelector('.score').textContent =
+	 0;
+
+} else if (guess < secretNumber) {
+	if (score > 1) {
+ 	document.querySelector('.message'). textContent
+ 	= 'I Too low!';
+  	score--;
+ 
+ document.querySelector('.score').textContent = score;
+ } else {
+ 	document.querySelector('.message'). textContent = 
+     'You lost the game!';
+  	document.querySelector('.score').textContent =
+  	0;	 
 
 }
