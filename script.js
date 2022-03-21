2ee512e6349b711d68038ca7324c5d297b34d884
JohnAntuch <johnantuch@gmail.com>
2022-03-21 10:48:48 -0300

    Update script.js
    Adding source code to challenge

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const secretNumber = Math.trunc (Math.random() * 20) +
+1;
+
+let score = 20;
+             
+document.querySelector('.check').addEventlistener('click', function ()) {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+  	document.querySelector('.message').textContent = 
+    'No number!';
+
+} else if (guess === secretNumber) {
+	document.querySelector('.message').textContent
+	'Correct Number!';
+	
+	document.querySelector('.number').textContent =
+	secretNumber;
+	
+	document.querySelector('body').style.
+	backgroundColor = '#60b347';
+	
+	document.querySelector('.number').style.width =
+	'30rem';
+}
+
+}
